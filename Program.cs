1ada6702817208d96fb542a991ad91c3bec29513
Bartłomiej Kłuś <97251883+Onestyle7@users.noreply.github.com>
2023-04-28 20:52:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,35 +5,36 @@ namespace Pudelko
         static void Main(string[] args)
         {
             Pudelko p1 = new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter);
-            Console.WriteLine(p1.ToString());
-            Console.WriteLine(p1.ToString("m"));
-            Console.WriteLine(p1.ToString("cm"));
-            Console.WriteLine(p1.ToString("mm"));
-            Console.WriteLine(p1.ToString("m3"));
-            Console.WriteLine(p1.ToString("m2"));
-            Console.WriteLine("");
-            Pudelko p2 = new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter);
-            Console.WriteLine($"Pudelko p2 = {p2}");
+             Console.WriteLine(p1.ToString());
+             Console.WriteLine(p1.ToString("m"));
+             Console.WriteLine(p1.ToString("cm"));
+             Console.WriteLine(p1.ToString("mm"));
+             Console.WriteLine(p1.ToString("m3"));
+             Console.WriteLine(p1.ToString("m2"));
+             Console.WriteLine("");
+             Pudelko p2 = new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter);
+             Console.WriteLine($"Pudelko p2 = {p2}");
 
-            Console.WriteLine($"p1 == p2 ? {p1 == p2}");
-            Console.WriteLine($"p1 != p2 ? {p1 != p2}");
+             Console.WriteLine($"p1 == p2 ? {p1 == p2}");
+             Console.WriteLine($"p1 != p2 ? {p1 != p2}");
 
-            Pudelko p3 = new Pudelko(1, 2.1, 3.05, UnitOfMeasure.meter);
-            Console.WriteLine($"Pudelko p3 = {p3}");
+             Pudelko p3 = new Pudelko(1, 2.1, 3.05, UnitOfMeasure.meter);
+             Console.WriteLine($"Pudelko p3 = {p3}");
 
-            Console.WriteLine($"p1 == p3 ? {p1 == p3}");
-            Console.WriteLine($"p1 != p3 ? {p1 != p3}");
+             Console.WriteLine($"p1 == p3 ? {p1 == p3}");
+             Console.WriteLine($"p1 != p3 ? {p1 != p3}");
 
-
+            
         }
     }
+
     enum UnitOfMeasure
     {
         milimeter,
         centimeter,
         meter
     }
-    class Pudelko : IEquatable<Pudelko>
+    class Pudelko : IEquatable<Pudelko>, IEnumerator<double>
     {
         private double a, b, c;
         private UnitOfMeasure unit;
@@ -49,9 +50,39 @@ namespace Pudelko
             this.c = c;
             this.unit = unit;
         }
+        public static Pudelko operator +(Pudelko p1, Pudelko p2)
+        {
+            double outputA, outputB, outputC;
+            double[] dimensionFirst = { p1.A, p1.A, p1.B };
+            double[] dimensionSecond = { p2.A, p2.A, p2.B };
+            Array.Sort(dimensionFirst);
+            Array.Sort(dimensionSecond);
+
+            if (dimensionFirst[2] > dimensionSecond[2])
+                outputA = dimensionFirst[2];
+            else
+                outputA = dimensionSecond[2];
+
+            if (dimensionFirst[1] > dimensionSecond[1])
+                outputB = dimensionFirst[1];
+            else
+                outputB = dimensionSecond[1];
+
+            outputC = dimensionFirst[0] + dimensionSecond[0];
+
+            return new Pudelko(outputA, outputB, outputC);
+
+        }
         public double A => a / 100.0;
         public double B => b / 100.0;
         public double C => c / 100.0;
+        public IEnumerator<double> GetEnumerator()
+        {
+            yield return A;
+            yield return B;
+            yield return C;
+        }
+
         public override string ToString()
         {
             string unitString = "";
@@ -94,7 +125,7 @@ namespace Pudelko
                 case "m2":
                     unitString = "m2";
                     factor = 1;
-                    return $"{a*b * factor:F6} {unitString}";
+                    return $"{a * b * factor:F6} {unitString}";
                 default:
                     throw new FormatException("Nieznany format.");
             }
@@ -102,12 +133,16 @@ namespace Pudelko
         public double Objetosc => Math.Round(a * b * c);
         public double Pole => Math.Round(a * b);
 
+        public double Current => throw new NotImplementedException();
+
+        object System.Collections.IEnumerator.Current => throw new NotImplementedException();
+
         public bool Equals(Pudelko other)
         {
             if (other == null) return false;
 
-            var sortedThis = new[] {a,b,c }.OrderBy(x => x).ToArray();
-            var sortedOther = new[] {other.a,other.b,other.c }.OrderBy(x => x).ToArray();
+            var sortedThis = new[] { a, b, c }.OrderBy(x => x).ToArray();
+            var sortedOther = new[] { other.a, other.b, other.c }.OrderBy(x => x).ToArray();
 
             return sortedThis[0] == sortedOther[0] && sortedThis[1] == sortedOther[1] && sortedThis[2] == sortedOther[2];
         }
@@ -131,6 +166,65 @@ namespace Pudelko
         public static bool operator !=(Pudelko p1, Pudelko p2)
         {
             return !(p1 == p2);
+        }
+        public static explicit operator double[](Pudelko p)
+        {
+            return new double[] { p.A, p.B, p.C };
+        }
+        public static implicit operator Pudelko((int A, int B, int C) dimensions)
+        {
+            return new Pudelko(dimensions.A, dimensions.B, dimensions.C);
+        }
+        public static Pudelko Parse(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input)) throw new ArgumentException(input);
+
+            string[] inputWords = input.Split(" × ");
+
+            double[] inputDimensions = { 100, 100, 100 };
+
+            if (inputWords.Length > 3) throw new FormatException("Format przekroczył trzeci wymiar");
+
+            for (int i = 0; i < inputWords.Length; i++)
+                if (inputWords[i] is not null)
+                {
+                    string[] word = inputWords[i].Split(' ');
+
+                    int factor;
+
+                    if (word.Length == 1) factor = 1000;
+                    else if (word[1].ToUpper() == "M")
+                        factor = 1000;
+                    else if (word[1].ToUpper() == "CM")
+                        factor = 10;
+                    else if (word[1].ToUpper() == "MM")
+                        factor = 1;
+                    else
+                        throw new FormatException($"Format {word[1]} nie jest wspierany");
+
+                    bool conversionSuccess = double.TryParse(word[0], out double parsedValue);
+
+                    if (!conversionSuccess) throw new FormatException("Konwersja nie powiodła się. Ciąg wejściowy nie został poprawnie sformatowany");
+
+                    inputDimensions[i] = parsedValue * factor;
+                }
+
+            return new Pudelko(inputDimensions[0], inputDimensions[1], inputDimensions[2], UnitOfMeasure.milimeter);
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 
